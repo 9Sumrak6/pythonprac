600f022f36d294ed82b7bafd67e4011c364333c8
Carbon6090 <osotoff.stepan@yandex.ru>
2023-10-08 21:17:29 +0300

    Error correction

---
+++ 20231003/3/prog.py
@@ -0,0 +1,13 @@
+from math import *
+
+def Calc(s, t, u):
+	u = 'lambda x, y:' + u
+	s = 'lambda x:' + s
+	t = 'lambda x:' + t
+
+	def h(x):
+		return eval(u)(eval(s)(x), eval(t)(x))
+
+	return h
+
+print(Calc(*eval(input()))(eval(input())))
