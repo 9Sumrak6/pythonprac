55275232c3281abe7e4d65800ec22cec86b95d77
Carbon6090 <osotoff.stepan@yandex.ru>
2023-10-09 22:44:37 +0300

    Add tests to 4 hw

--- 20231003/1/prog.py
+++ 20231003/1/prog.py
@@ -1,19 +1,21 @@
 def Pareto(*args):
-	ans = []
+    if not isinstance(args[0], tuple):
+        return args[0], args[1]
 
-	for i in range(len(args)):
-		fl = True
-		for j in range(len(args)):
-			if i == j:
-				continue
-			
-			if args[j][0] >= args[i][0] and args[j][1] >= args[i][1] and (args[j][0] > args[i][0] or args[j][1] > args[i][1]):
-				fl = False
-				break
+    ans = []
+    for i in range(len(args)):
+        fl = True
+        for j in range(len(args)):
+            if i == j:
+                continue
 
-		if fl:
-			ans.append(args[i])
+            if args[j][0] >= args[i][0] and args[j][1] >= args[i][1] and (args[j][0] > args[i][0] or args[j][1] > args[i][1]):
+                fl = False
+                break
 
-	return tuple(ans)
+        if fl:
+            ans.append(args[i])
+
+    return tuple(ans)
 
 print(Pareto(*eval(input())))
