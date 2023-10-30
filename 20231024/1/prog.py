ede16bb7b4d99998913d09ef54963abd658848c4
Carbon6090 <osotoff.stepan@yandex.ru>
2023-10-30 22:47:27 +0300

    Fix bugs

--- 20231024/1/prog.py
+++ 20231024/1/prog.py
@@ -1,18 +1,17 @@
 import sys
 
 
-class C:
-	def fib(self, m, n):
-		a, b = 0, 1
+def fib(self, m, n):
+	a, b = 0, 1
 
-		if m > n:
-			return
+	if m > n:
+		return
 
-		for i in range(m):
-			a, b = b, a + b
+	for i in range(m):
+		a, b = b, a + b
 
-		for i in range(n):
-			yield b
-			a, b = b, a + b
+	for i in range(n):
+		yield b
+		a, b = b, a + b
 
 exec(sys.stdin.read())
--- 20231024/2/prog.py
+++ 20231024/2/prog.py
@@ -2,16 +2,15 @@ import itertools
 import sys
 
 
-class C:
-    def slide(self, seq, n):
-        check, ite, cur = itertools.tee(seq, 3)
-        
-        for i in itertools.count(0):
-            if len(list(itertools.islice(check, i, i + n))) == 0:
-                return
+def slide(self, seq, n):
+    check, ite, cur = itertools.tee(seq, 3)
+    
+    for i in itertools.count(0):
+        if len(list(itertools.islice(check, i, i + n))) == 0:
+            return
 
-            yield from itertools.islice(ite, i, i + n)
+        yield from itertools.islice(ite, i, i + n)
 
-            check, ite, cur = itertools.tee(cur, 3)
+        check, ite, cur = itertools.tee(cur, 3)
 
 exec(sys.stdin.read())
