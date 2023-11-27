5d61d328799bad48cddd2c07d7cb4a74c1e436f7
Step <osotoff.stepan@yandex.ru>
2023-11-28 01:19:05 +0300

    Fix bugs in hw

--- 20231121/3/prog.py
+++ 20231121/3/prog.py
@@ -6,5 +6,26 @@ l = len(raw)
 
 if l < 44 or raw[:4] != b'RIFF' or raw[8:16] != b'WAVEfmt ' or raw[36:40] != b'data':
     print('NO')
-else:
-    print(f'Size={int.from_bytes(raw[4:8], 'little')}, Type={int.from_bytes(raw[20:22], 'little')}, Channels={int.from_bytes(raw[22:24], 'little')}, Rate={int.from_bytes(raw[24:28], 'little')}, Bits={int.from_bytes(raw[34:36], 'little')}, Data size={int.from_bytes(raw[40:44], 'little')}')
+    exit(0)
+
+t1 = struct.unpack("i", raw[16:20])[0]
+t3 = struct.unpack("h", raw[32:34])[0]
+t2 = struct.unpack("i", raw[28:32])[0]
+
+c = struct.unpack("h", raw[22:24])[0]
+r = struct.unpack("i", raw[24:28])[0]
+b = struct.unpack("h", raw[34:36])[0]
+
+if t1 != 16 or t2 != (r * b * c) / 8 or t3 != (b * c) / 8 or t3 not in {1, 2, 4}:
+    print('NO')
+    exit(0)
+
+s = struct.unpack("i", raw[4:8])[0]
+t = struct.unpack("h", raw[20:22])[0]
+d = struct.unpack("i", raw[40:44])[0]
+
+if s != l - 8 or r not in {44100, 4800} or d != s - 36:
+    print('NO')
+    exit(0) 
+
+print(f'Size={s}, Type={t}, Channels={c}, Rate={r}, Bits={b}, Data size={d}')
